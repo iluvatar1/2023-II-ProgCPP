bcd09211483854e46a74a1de041bd3b8ee2092bd
iluvatar1 <woquendo@gmail.com>
2023-09-15 10:54:00 -0500

    Added errors exploration including richardson, using pointers for deriv

--- 2023-09-15-derivatives/deriv_utils.h
+++ 2023-09-15-derivatives/deriv_utils.h
@@ -2,7 +2,7 @@
 #include <cmath>
 
 using fptr = double(double); // puntero a funcion
+using algptr = double(double, double, fptr);
 double forward_diff(double x, double h, fptr f);
 double central_diff(double x, double h, fptr f);
-double richardson_forward(double x, double h, fptr f, int order);
-double richardson_central(double x, double h, fptr f, int order);
+double richardson(double x, double h, fptr f, int order, algptr alg);
--- 2023-09-15-derivatives/main_richardson.cpp
+++ 2023-09-15-derivatives/main_richardson.cpp
@@ -16,8 +16,8 @@ int main(int argc, char **argv)
     const double dexact = std::cos(x*x)*2*x;
     double diff_f = std::fabs(1-forward_diff(x, h, fun)/dexact);
     double diff_c = std::fabs(1-central_diff(x, h, fun)/dexact);
-    double diff_rf = std::fabs(1-richardson_forward(x, h, fun, 1)/dexact);
-    double diff_rc = std::fabs(1-richardson_central(x, h, fun, 2)/dexact);
+    double diff_rf = std::fabs(1-richardson(x, h, fun, 1, forward_diff)/dexact);
+    double diff_rc = std::fabs(1-richardson(x, h, fun, 2, central_diff)/dexact);
     fout << h << "\t" << fun(x) << "\t"
 	 << diff_f << "\t" << diff_c << "\t"
 	 << diff_rf << "\t" << diff_rc << "\t"
